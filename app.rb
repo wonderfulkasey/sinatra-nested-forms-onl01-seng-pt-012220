1d75d5a2686ae8a406861b32fec30165f90949d9
Kasey Arnold <kaseyarnold15@gmail.com>
2020-04-20 19:32:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,22 +8,22 @@ module FormsLab
     get '/' do
           erb :root
         end
-    
+
         get '/new' do
           erb :'pirates/new'
         end
-    
+
         post '/pirates' do
           @pirate = Pirate.new(params[:pirate])
-    
+
           params[:pirate][:ships].each do |details|
             Ship.new(details)
           end
           @ships = Ship.all
-    
+
           erb :'pirates/show'
         end
-        
+
 
   end
 end
